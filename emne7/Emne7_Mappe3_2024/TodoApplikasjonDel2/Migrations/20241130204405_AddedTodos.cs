a35db494f26a18f45cfe994e310e788f41b399ad
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-01-13 19:42:14 +0100

    last

---
+++ emne7/Emne7_Mappe3_2024/TodoApplikasjonDel2/Migrations/20241130204405_AddedTodos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApplikasjon.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTodos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
