a6019b93989e196764d1a520f9160d9372fe2c57
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-02-23 23:24:23 +0100

    last

---
+++ emne7/TodoApplikasjon/Services/ITodosService.cs
@@ -0,0 +1,15 @@
+using TodoApplikasjon.Models;
+
+public interface ITodosService
+{
+    IEnumerable<Todo> GetTodos();           //Henter alle todos
+    Todo? GetTodoById(int id);              //henter en todo med id
+
+    void AddTodo(Todo todo);                //legger en todo
+
+    void UpdateTodo(int id, Todo todo);     //oppdater eksisterende todo
+
+    void DeleteTodo(int id);                //sletter en todo basert på id
+
+
+}
