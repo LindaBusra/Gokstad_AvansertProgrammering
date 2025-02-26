d450b0df2a71297b219d9a43bb3536682c16840c
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-02-26 23:25:55 +0100

    last

---
+++ Lecture4/Migrations/20241130144009_Addedusers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lecture4.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedusers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
