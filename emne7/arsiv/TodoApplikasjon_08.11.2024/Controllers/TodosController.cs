e2f72204a5c82d698661ecf4dc0dcb9f190612ee
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-03-03 22:53:56 +0100

    last

---
+++ emne7/arsiv/TodoApplikasjon_08.11.2024/Controllers/TodosController.cs
@@ -0,0 +1,90 @@
+using TodoApplikasjon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Todo = TodoApplikasjon.Models.Todo;
+
+namespace TodoApplikasjon.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class TodosController : ControllerBase
+    {
+        public static List<Todo> todos = new List<Todo>
+        {
+            new Todo {Id=1, Title="Todo1", Description="Create a home page", IsCompleted=true},
+            new Todo {Id=2, Title="Todo2", Description="Create about us page", IsCompleted=true}
+        };   
+        
+ 
+
+    //GET /api/todos: Hent alle Todo-oppgaver.
+    [HttpGet]
+    public ActionResult<IEnumerable<Todo>> GetTodos()
+    {
+        return Ok(todos);
+    }
+
+    //GET /api/todos/{id}: Hent en spesifikk Todo-oppgave basert på ID.
+    [HttpGet("{id}")]
+    public ActionResult<IEnumerable<Todo>> GetTodoById(int id)
+    {
+        var todo = todos.FirstOrDefault (b => b.Id == id) ;
+        if(todo == null)
+        {
+            return NotFound();
+        }
+        return Ok(todo);
+    }
+
+
+    //POST /api/todos: Opprett en ny Todo-oppgave.
+    [HttpPost]
+    public ActionResult<Todo> CreateTodo(Todo newTodo)
+    
+    {
+        if (!ModelState.IsValid)  
+        {
+        return BadRequest(ModelState);  // this returns 400 Bad Request and error details
+        }
+
+        newTodo.Id = todos.Max(b => b.Id )+1;
+        todos.Add(newTodo);
+        return CreatedAtAction(nameof(GetTodoById), new { id = newTodo.Id }, newTodo);
+    }
+
+
+
+    //PUT /api/todos/{id}: Oppdater en Todo-oppgave basert på ID.
+    [HttpPut("{id}")]
+    public ActionResult UpdateTodo(int id, Todo updateTodo)
+    {
+
+        var existingTodo = todos.FirstOrDefault (b => b.Id == id);
+        if(existingTodo == null)
+        {
+            return NotFound();
+        }
+        existingTodo.Title = updateTodo.Title;
+        existingTodo.Description = updateTodo.Description;
+        existingTodo.IsCompleted = updateTodo.IsCompleted;
+        return NoContent();
+    }
+
+
+    //DELETE /api/todos/{id}: Slett en Todo-oppgave basert på ID.
+    [HttpDelete("{id}")]
+    public ActionResult DeleteTodo(int id)
+    {
+        var todo = todos.FirstOrDefault(b=>b.Id==id);
+        if(todo == null)
+        {
+            return NotFound();
+        }
+
+        todos.Remove(todo);
+        return NoContent();
+    }
+
+
+    }
+}
